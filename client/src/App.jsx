3933537508fabe3e4ccf865007bd465a7116b91c
manikandan <mani.rajavertrivel@gmail.com>
2025-08-09 19:56:02 +0530

    cart fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from './components/login';
 import Signup from './components/signup';
+import ProductList from './components/ProductList';
+import CartSummary from './components/CartSummary';
+import { CartProvider } from './context/CartContext';
+
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </Router>
+      <CartProvider>
+        <Router>
+          <header className="header-bar">
+            <div className="header-left">🛍️ MyShop</div>
+            <nav className="nav">
+              <Link to="/catalog">Catalog</Link>
+              <Link to="/cart">Cart</Link>
+            </nav>
+          </header>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/catalog" element={<ProductList />} />
+            <Route path="/cart" element={<CartSummary />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
   )
 }
